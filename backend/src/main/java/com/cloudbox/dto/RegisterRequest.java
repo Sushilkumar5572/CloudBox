89b9c96dff8e2904c84d85d8ed555770e9773f06
Pranay Makkena <pranaymakkena@gmail.com>
2026-03-16 23:01:15 +0530

    Update RegisterRequest.java

--- backend/src/main/java/com/cloudbox/dto/RegisterRequest.java
+++ backend/src/main/java/com/cloudbox/dto/RegisterRequest.java
@@ -1,39 +1,16 @@
 package com.cloudbox.dto;
 
-/*
- Request body for register API
-*/
+import lombok.Data;
 
+@Data
 public class RegisterRequest {
 
-    private String name;
-    private String email;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private int age;
+    private String location;
     private String password;
+    private String confirmPassword;
 
-    public RegisterRequest() {
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
